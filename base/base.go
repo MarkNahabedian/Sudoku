9ac224b95d482193a88bde7652ada34b0bf51323
Markk Nahabedian <naha@mit.edu>
2017-12-11 14:43:06 -0500

    New constraint model.

--- base/base.go
+++ base/base.go
@@ -1,29 +1,38 @@
+// Package base implements the basic data model and functionality for
+// a sudoku solver.
 package base
 
 import "fmt"
 import "io"
 import "os"
-import "reflect"
-import "runtime"
 
+// Contradiction is the type of error that is returned if an operation
+// results in a contradiction.
 type Contradiction struct {
+	// Cell is the Cell that's the subject of the contradiction.
 	Cell *Cell
+	// Issue is the error message describing the contradiction.
 	Issue string
 }
 
+// Error implements the error interface.
 func (c *Contradiction) Error() string {
 	return fmt.Sprintf("Contradiction at [%d, %d]: %s", c.Cell.X, c.Cell.Y, c.Issue)
 }
 
+// GridKey represents a location in a Puzzle's cell grid.
 type GridKey struct {
 	X int
 	Y int
 }
 
+// MakeGridKey returns a GridKey for the specified X and Y position.
 func MakeGridKey(x int, y int) GridKey {
 	return GridKey{ X: x, Y: y }
 }
 
+// Cell represents a single cell of a Puzzle.  When the Puzzle is solved,
+// each of its Cells will contain a single value.
 type Cell struct {
 	X int
 	Y int
@@ -32,15 +41,25 @@ type Cell struct {
 	Possibilities ValueSet
 }
 
+// Puzzle represents a single sudoku puzzle.
 type Puzzle struct {
+	// Size is the numbner of rows and columns in the Puzzle grid.
 	Size int
+    // Grid is the grid of cells.
 	Grid map[GridKey]*Cell
-	Progress uint  // should be increased whenever progrress is made
+    // Progress is a monotonically increasing integer.
+	// It increases whenever progress is made.
+	Progress uint
+	// Groups is a slice conbtaining all of the Groups of this Puzzle.
 	Groups []*Group
+	// Universe is a ValueSet of all of the values that could appear in any Cell.
 	Universe ValueSet
+	// Justifications is a slice of all of the Justifications for what's
+	// been asserted about this Puzzle.
 	Justifications []*Justification
 }
 
+// Cell returns the Cell at the specified x and y position.
 func (p *Puzzle) Cell(x, y int) *Cell {
 	c := p.Grid[MakeGridKey(x, y)]
 	if c == nil {
@@ -200,14 +219,34 @@ func (p *Puzzle) DoConstraints() error {
 	return nil
 }
 
-type Constraint func(*Group) error
-
-func (constraint Constraint) Name() string {
-	return runtime.FuncForPC(reflect.ValueOf(constraint).Pointer()).Name()
+// Constraint implements a Constraint that is to be enforced on
+// a Group of Cells.
+type Constraint interface {
+	Name() string
+	DoConstraint(*Group) error
 }
 
-func Given(g *Group) error { return nil }
+type FunctionConstraint struct {
+	 name string
+	 constraint func(*Group) error
+}
 
+func (constraint FunctionConstraint) Name() string {
+	return constraint.name
+}
+
+func (constraint FunctionConstraint) DoConstraint(g *Group) error {
+	return constraint.constraint(g)
+}
+
+var Given = FunctionConstraint {
+	name: "Given",
+	constraint: (func(g *Group) error { return nil }),
+}
+
+// Group represents a set of cells to which some constraint collectively
+// applies, for example a row in a sudoku whose cells can not contain
+// matching values.
 type Group struct {
 	puzzle *Puzzle
 	cells []*Cell
@@ -218,62 +257,80 @@ func (g *Group) Puzzle() *Puzzle { return g.puzzle }
 
 func (g *Group) Cells() []*Cell { return g.cells }
 
+// DoConstraints applies the Group's constraints.
 func (g *Group) DoConstraints() error {
 	for _, constraint := range g.constraints {
-		if err := constraint(g); err != nil {
+		if err := constraint.DoConstraint(g); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func HereThenNotElsewhereConstraint(g *Group) error {
-	// If there are n cells in the group with n possibilities and those
-	// possibilities are the same then none of the other cells in the group
-	// can have any of those values.
-	for i, c1 := range g.Cells() {
-		if c1.Possibilities.Len() > c1.Puzzle.Universe.Len() - i {
-			continue
-		}
-		count := 0  // How many of the remaining cells have the same Possibilities
-		for _, c2 := range g.Cells()[i:] {
-			if c1.Possibilities == c2.Possibilities {
-				count += 1
+// HereThenNotElsewhereConstraint implements the constraint that says that
+// the value of one Cell of a Group can not appear in any other Cell of
+// that Group.
+var HereThenNotElsewhereConstraint FunctionConstraint
+
+func init() {
+	HereThenNotElsewhereConstraint.name = "HereThenNotElsewhereConstraint"
+	HereThenNotElsewhereConstraint.constraint = func(g *Group) error {
+		// If there are n cells in the group with n possibilities and those
+		// possibilities are the same then none of the other cells in the group
+		// can have any of those values.
+		for i, c1 := range g.Cells() {
+			if c1.Possibilities.Len() > c1.Puzzle.Universe.Len() - i {
+				continue
 			}
-		}
-		if count == c1.Possibilities.Len() {
-			for _, c3 := range g.Cells() {
-				if c3.Possibilities != c1.Possibilities {
-					c1.Possibilities.DoValues(func(val int) {
-						c3.CantBe(val, HereThenNotElsewhereConstraint)
-					})
+			count := 0  // How many of the remaining cells have the same Possibilities
+			for _, c2 := range g.Cells()[i:] {
+				if c1.Possibilities == c2.Possibilities {
+					count += 1
+				}
+			}
+			if count == c1.Possibilities.Len() {
+				for _, c3 := range g.Cells() {
+					if c3.Possibilities != c1.Possibilities {
+						c1.Possibilities.DoValues(func(val int) {
+							c3.CantBe(val, HereThenNotElsewhereConstraint)
+						})
+					}
 				}
 			}
 		}
+		return nil
 	}
-	return nil
 }
 
-func NotElsewhereThenHereConstraint(g *Group) error {
-	// If a value can't be in all but one cell of a group then the one
-	// cell that can have that value must have it.
-	// Is there a generalization of this for several values?
-	valueCells := make(map[int][]*Cell)
-	for _, c := range g.Cells() {
-		c.Possibilities.DoValues(func (v int) {
-			valueCells[v] = append(valueCells[v], c)
-		})
-	}
-	for v, cells := range valueCells {
-		if len(cells) == 1 {
-			if _, err := cells[0].MustBe(v, NotElsewhereThenHereConstraint); err != nil {
-				return err
+// NotElsewhereThenHereConstraint implements the constraint that says
+// that each value must appear in some Cell of the Group.
+var NotElsewhereThenHereConstraint FunctionConstraint
+
+func init() {
+	NotElsewhereThenHereConstraint.name = "NotElsewhereThenHereConstraint"
+	NotElsewhereThenHereConstraint.constraint = func(g *Group) error {
+		// If a value can't be in all but one cell of a group then the one
+		// cell that can have that value must have it.
+		// Is there a generalization of this for several values?
+		valueCells := make(map[int][]*Cell)
+		for _, c := range g.Cells() {
+			c.Possibilities.DoValues(func (v int) {
+				valueCells[v] = append(valueCells[v], c)
+			})
+		}
+		for v, cells := range valueCells {
+			if len(cells) == 1 {
+				if _, err := cells[0].MustBe(v, NotElsewhereThenHereConstraint); err != nil {
+					return err
+				}
 			}
 		}
+		return nil
 	}
-	return nil
 }
 
+// AddLinerGroups adds the conventional sudoku row and column constraints
+// to the Puzzle.
 func (p *Puzzle) AddLineGroups() *Puzzle {
 	addGroup := func(cells []*Cell) {
 		g := &Group{
@@ -303,6 +360,7 @@ func (p *Puzzle) AddLineGroups() *Puzzle {
 	return p
 }
 
+// Add3x3Groups implements the small 3x3 box constraints of a sudoku.
 func (p *Puzzle) Add3x3Groups() *Puzzle {
 	for sx := 1; sx < 9; sx += 3 {
 		for sy := 1; sy < 9; sy += 3 {
