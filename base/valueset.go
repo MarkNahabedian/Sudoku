efc4a06e798d1d37a608285948adbd422e4097b1
Markk Nahabedian <naha@mit.edu>
2017-12-14 16:02:32 -0500

    Union, Intersection, SetDifference, Get. Incompatible change to DoValues.

--- base/valueset.go
+++ base/valueset.go
@@ -1,9 +1,11 @@
 package base
 
+// ValueSet represents a set of values that appear in a sudoku Cell.
 type ValueSet uint16
 
 const MaxValue = 9
 
+// NewValueSet returns a new ValueSet that contains every value in universe.
 func NewValueSet(universe []int) ValueSet {
 	var vs ValueSet = 0
 	for _, v := range universe {
@@ -12,10 +14,12 @@ func NewValueSet(universe []int) ValueSet {
 	return vs
 }
 
+// IsEmpty returns true if the ValueSet contains no values.
 func (vs ValueSet) IsEmpty() bool {
 	return vs == 0
 }
 
+// Len returns the number of values in the ValueSet.
 func (vs ValueSet) Len() int {
 	count := 0
 	for i := 1; i <= MaxValue; i++ {
@@ -30,14 +34,16 @@ func bitmask(v int) ValueSet {
 	if v < 1 {
 		return 0
 	}
-	return 1 << uint16(v - 1)
+	return 1 << uint16(v-1)
 }
 
+// HasValue returns true of vs contains v.
 func (vs ValueSet) HasValue(v int) bool {
 	m := bitmask(v)
-	return vs & m == m
+	return vs&m == m
 }
 
+// SetHasValue returns a new ValueSet with v adde3d to vs.
 func (vs ValueSet) SetHasValue(v int, has bool) ValueSet {
 	if has {
 		return vs | bitmask(v)
@@ -46,10 +52,52 @@ func (vs ValueSet) SetHasValue(v int, has bool) ValueSet {
 	}
 }
 
-func (vs ValueSet) DoValues(f func(int)) {
+// Union eturns the union of two value sets.
+func (vs1 ValueSet) Union(vs2 ValueSet) ValueSet {
+	return vs1 | vs2
+}
+
+// Intersection eturns the intersection of two value sets.
+func (vs1 ValueSet) Intersection(vs2 ValueSet) ValueSet {
+	return vs1 & vs2
+}
+
+// SetDifference returns a ValueSet containing those values in vs1 which are not in vs2.
+func (vs1 ValueSet) SetDifference(vs2 ValueSet) ValueSet {
+	return vs1 & ^vs2
+}
+
+// DoValues calls f on each value in the ValueSet.  If f returns false
+// then DoValues doen't call it on further values but returns immediately.
+func (vs ValueSet) DoValues(f func(int) bool) {
 	for v := 1; v <= MaxValue; v++ {
 		if vs.HasValue(v) {
-			f(v)
+			if !f(v) {
+				return
+			}
 		}
 	}
+}
+
+// Get returns the indexth value from the ValueSet.  Get panics if index
+// is out of range (0 based).
+func (vs ValueSet) Get(index int) int {
+	count := 0
+	value := -1
+	if index < 0 {
+		panic("Index out of range")
+	}
+	if index >= vs.Len() {
+		panic("Index out of range")
+	}
+	f := func(v int) bool {
+		if index == count {
+			value = v
+			return false
+		}
+		count += 1
+		return true
+	}
+	vs.DoValues(f)
+	return value
 }
--- base/valueset_test.go
+++ base/valueset_test.go
@@ -2,11 +2,11 @@ package base
 
 import "testing"
 
-func TestValueSet(t *testing.T) {
+func TestValueSetHas(t *testing.T) {
 	values := []int{2, 3}
 	vs := NewValueSet(values)
 	t.Logf("vs = %03x", vs)
-	expectValue := func (vs ValueSet, v int, has bool) {
+	expectValue := func(vs ValueSet, v int, has bool) {
 		if h := vs.HasValue(v); h != has {
 			t.Errorf("HasValue(%d): want %v, got %v", v, has, h)
 		}
@@ -26,10 +26,27 @@ func TestValueSet(t *testing.T) {
 	if vs1 := NewValueSet([]int{4}); vs1 != 8 {
 		t.Errorf("NewValueSet returned empty")
 	}
+}
 
-	vs = NewValueSet(values)
+func TestSetOperations(t *testing.T) {
+	vs1 := NewValueSet([]int{1, 3, 4})
+	vs2 := NewValueSet([]int{1, 2, 4})
+	if want, got := NewValueSet([]int{1, 2, 3, 4}), vs1.Union(vs2); want != got {
+		t.Errorf("Union failed, want %x, got %x", want, got)
+	}
+	if want, got := NewValueSet([]int{1, 4}), vs1.Intersection(vs2); want != got {
+		t.Errorf("Intersection failed, want %x, got %x", want, got)
+	}
+	if want, got := NewValueSet([]int{3}), vs1.SetDifference(vs2); want != got {
+		t.Errorf("SetDifference failed, want %x, got %x", want, got)
+	}
+}
+
+func TestDoValues(t *testing.T) {
+	values := []int{2, 3}
+	vs := NewValueSet(values)
 	valueIndex := 0
-	vs.DoValues(func(v int) {
+	vs.DoValues(func(v int) bool {
 		t.Logf("DoValues function got %d from %03x", v, vs)
 		if valueIndex < len(values) {
 			if want := values[valueIndex]; want != v {
@@ -37,8 +54,21 @@ func TestValueSet(t *testing.T) {
 			}
 		}
 		valueIndex += 1
+		return true
 	})
 	if valueIndex != len(values) {
 		t.Errorf("DoValues function called %d times, expected %d times", valueIndex, len(values))
 	}
+}
+
+func TestGet(t *testing.T) {
+	values := []int{2, 4}
+	vs := NewValueSet(values)
+	test_index := func(index int) {
+		if want, got := values[index], vs.Get(index); got != want {
+			t.Errorf("At index %d: expected %d, got %d.", index, want, got)
+		}
+	}
+	test_index(0)
+	test_index(1)
 }
